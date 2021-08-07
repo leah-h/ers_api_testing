69dd81ca2bce7672fe556e39962722ff95a51776
Leah H <harrichl@gmail.com>
2021-08-07 17:35:35 -0400

    test add reimbursement endpoint

--- cypress/integration/api_spec.js
+++ cypress/integration/api_spec.js
@@ -1,9 +1,28 @@
-describe('GET all reimbursements', () => {
+describe('ERS Reimbursements Tests', () => {
+
+    Cypress.config('baseUrl', 'http://localhost:7000/reimbursements')
+
     it("GET all", () => {
-        cy.request('http://localhost:7000/reimbursements').then(response => {
+        cy.request('/').then(response => {
             expect(response).to.have.property('status', 200)
             expect(response.body).to.not.be.null
-            expect(response.body).to.have.length(13)
+            expect(response.body).to.have.length(19)
         })
     })
+
+    it("Add Reimbursement", () => {
+        const reimbursement = {
+            "amount": 5000.00,
+            "description": "airline tickets",
+            "submitted": "2021-05-01",
+            "author": 1002,
+            "resolver": null,
+            "resolved": null,
+            "typeId": 1,
+            "statusId": 1,
+            "receipt": "should be a file address...."
+        }
+        cy.request('POST', '/', reimbursement)
+            .its("body")
+        })
 })
